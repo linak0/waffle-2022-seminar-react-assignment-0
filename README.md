034f62928fc1b0292344c4eb136178237eb029ad
linak0 <sspghj@snu.ac.kr>
2022-09-02 17:12:09 +0900

    주석

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # waffle-2022-seminar-react-assignment-0
 
-## test
+## 스펙에서 요구하지는 않았지만 
+## 입력 후 인풋 칸이 비워지도록 하였습니다.
--- asdf.js
+++ asdf.js
@@ -1,20 +1,24 @@
+
+//우상단 도움말 버튼
 var help = document.getElementById('help');
 
 help.addEventListener('click',function () {
     alert('입력창에 메뉴를 입력하고 추가 버튼을 클릭하거나 엔터를 입력해주세요.');
   })
 
-
+//메뉴입력 텍스트 클릭시 포커스 설정
 var text = document.getElementById('menu');
 text.addEventListener('click',function(){
   document.getElementById('input').focus();
 })
 
+//버튼 클릭시 추가
 var button = document.getElementById('button');
 button.addEventListener('click',function(event){
   addDiv();
 });
 
+//엔터 입력시 추가
 var input = document.getElementById('input');
 input.addEventListener('keydown',function(event){
   if(!event.isComposing && event.code== 'Enter'){
@@ -24,6 +28,7 @@ input.addEventListener('keydown',function(event){
   }
 });
 
+//추가 함수
 const addDiv = ()=>{
   var name = document.getElementById('input').value;
 
